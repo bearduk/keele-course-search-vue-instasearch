4ed1eca1f1be4e95055a5a474aa52b9798e5370e
Chris Beard <bearduk@users.noreply.github.com>
2017-08-03 11:29:45 +0100

    :🌅: Fleshed out basic search a bit. Results now have links, course level showing and I've used a few slots and attributes to override the defaults.

--- src/App.vue
+++ src/App.vue
@@ -4,14 +4,32 @@
     api-key="d37bbf3291b226676c9f3f1937e865d3"
     index-name="dev_COURSES"
   >
-    <ais-search-box></ais-search-box>
-    <ais-results>
-      <template scope="{ result }">
-        <h2>
-          <ais-highlight :result="result" attribute-name="courseTitle"></ais-highlight>
-        </h2>
+  <ais-input placeholder="inline placeholder"></ais-input>
+
+    <ais-search-box placeholder="no bind on placeholder text" :autofocus="true">
+    </ais-search-box>
+
+    <!-- cb overriding default content for no results found -->
+    <ais-no-results>
+      <template scope="props">
+      No courses found for <i>{{ props.query }}</i>.
       </template>
+    </ais-no-results>
+
+    <ais-results :results-per-page="5"> <!-- cb setting manually for now, just remove this for default -->
+      <template scope="{ result }">
+        <!-- <header><hr /></header> --> <!-- cb slot if needed -->
+        <p>
+          <a :href="result.urlPath">
+            <ais-highlight :result="result" attribute-name="courseTitle"></ais-highlight>
+          </a>
+          <span>{{result.courseLevel}}</span>
+        </p>        
+        <!-- <footer><hr /></footer> --> <!-- cb slot if needed -->
+      </template>
+
     </ais-results>
+    <ais-pagination></ais-pagination>
   </ais-index>
 </template>
 
@@ -19,6 +37,12 @@
 <script>
 export default {
   name: 'app'
+  // data() {
+  //   return {
+  //     sbplaceholder: 'chris bind placeholder',
+  //     somethingelse: ''
+  //   }
+  // }
 }
 </script>
 
